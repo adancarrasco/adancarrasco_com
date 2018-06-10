0d73fcbfb6f16d20b37266d3eef2be41a69afa34
Adán Carrasco <adan.carrasco@hotmail.com>
2018-06-09 23:08:24 -0600

    Fix routing for GitHub Pages.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import Router from './components/Router';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL} />, document.getElementById('root'));
