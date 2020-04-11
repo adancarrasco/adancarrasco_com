31c0fbabaf727c56ba75916277cab8f7604078ad
Adán Carrasco <adan.carrasco@hotmail.com>
2020-04-11 15:27:12 +0200

    Fix routing

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import App from '../App';
-import {ProjectContainer, NotFound} from './';
+import React from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import App from "../App"
+import { ProjectContainer, NotFound } from "./"
 
 const Router = () => (
   <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
     <Switch>
-      <Route exact path="/" render={ (routerProps) => <App {...this.props} />} />
+      <Route
+        exact
+        path="/"
+        render={(routerProps) => {
+          console.log(routerProps)
+          return <App {...this.props} history={routerProps.history} />
+        }}
+      />
       <Route path="/project/:projectId" component={ProjectContainer} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
-);
+)
 
-export default Router;
+export default Router
